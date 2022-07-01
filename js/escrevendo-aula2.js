ad856863c702d5b1c0540fd5b531bf86684fae09
WillianSampaio <willian.sampaio@df.estudante.senai.br>
2022-07-01 16:01:39 -0300

    atualizando

---
+++ js/escrevendo-aula2.js
@@ -0,0 +1,20 @@
+//o que é uma variável?
+/*
+  A variável é um espaço reservado na memória
+  do computador.
+  Vamos declarar variáveis no JavaScript?
+
+*/
+
+var button = document.querySelector("button");
+
+button.onclick = function () {
+
+  var nome = prompt("Qual é o comando para iniciar o git?");
+  if ((nome = "git init")) {
+    alert("O " + nome + ", serve para iniciar o git.Parabéns você acertou!");
+  }else{
+    alert("ERROOOOOOOU");
+  }
+  window.location="aula3.html";
+};
